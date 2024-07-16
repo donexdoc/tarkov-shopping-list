15c577429814c178beec1481f5f1986e1ef19f28
donex <donexdoc@gmail.com>
2024-07-16 21:36:14 +0300

    fix import

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import '@/App.css'
 import { AppLayout } from '@/layouts/AppLayout/AppLayout'
-// import PageController from '@/pages/PageController/PageController'
 import PageRouter from './pages/PageRouter/PageRouter'
 
 function App() {
